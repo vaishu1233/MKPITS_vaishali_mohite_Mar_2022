3347b5f643511b9fbbc427005489051e8c2ec539
vaishali mohite <101799368+vaishu1233@users.noreply.github.com>
2022-04-26 09:17:06 +0530

    Add files via upload

---
+++ VOS.C
@@ -0,0 +1,14 @@
+2) Volume of sphere= 4/3 * 3.14 * r * r * r
+  
+
+ #include<stdio.h>
+int main()
+{
+     int r;
+     float volume_sphere;
+     printf("Enter Radius : ");
+     scanf("%d",&r);
+     volume_sphere = (4/3.0)*3.14*r*r*r;
+     printf("\nVolume of Sphere = %f",volume_sphere);
+     return 0;
+} 
