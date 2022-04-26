bb7e7b8707f9820489bde4a92784a5e615f8bf08
vaishali mohite <101799368+vaishu1233@users.noreply.github.com>
2022-04-26 09:14:20 +0530

    Add files via upload

---
+++ COC.C
@@ -0,0 +1,14 @@
+ calculate circumference of circle
+
+
+#include<stdio.h>
+int main()
+{
+     int r;
+     float cir;
+     printf("Enter Radius : ");
+     scanf("%d",&r);
+     cir=2*3.14*r;
+     printf("\nCircumference of Circle = %.2f",cir);
+     return 0;
+}
