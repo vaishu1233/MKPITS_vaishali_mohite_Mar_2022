ec4a70744a33d8d7349c0ff97749e6aba2c71db7
vaishali mohite <101799368+vaishu1233@users.noreply.github.com>
2022-05-04 13:49:33 +0530

    Add files via upload

---
+++ comapre.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+int f=1;
+char n1[10]="ramesh";
+char n2[10]="ramesh1";
+for(i=0;n1[i]!='/0';i++)
+{
+if(n1[i]!=n2[i])
+{
+if=0;
+}
+}
+if(f==1)
+{
+string equal
+}
+else
+{
+not equal
+}
