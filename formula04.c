5029d32b8f039fb9bcb8a5a22f35808720025832
vaishali mohite <101799368+vaishu1233@users.noreply.github.com>
2022-05-08 19:39:31 +0530

    Add files via upload

---
+++ formula04.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+     int r;
+     float cir;
+     printf("Enter Radius : ");
+     scanf("%d",&r);
+     cir=2*3.14*r;
+     printf("\nCircumference of Circle = %.2f",cir);
+     return 0;
+}
