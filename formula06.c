02571eec8f2085d27e1a5c77d201b7e2ef19317f
vaishali mohite <101799368+vaishu1233@users.noreply.github.com>
2022-05-08 19:40:09 +0530

    Add files via upload

---
+++ formula06.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+     int side,squ_area;
+     printf("Enter Side : ");
+     scanf("%d",&side);
+     squ_area=side*side;
+     printf("\nArea of Square = %d",squ_area);
+     return 0;
+}
