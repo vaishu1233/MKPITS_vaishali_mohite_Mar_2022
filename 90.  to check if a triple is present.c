dd41d582c07b03029a910ea90299bf42ea5ba31e
vaishali mohite <101799368+vaishu1233@users.noreply.github.com>
2022-05-09 14:27:02 +0530

    Add files via upload

---
+++ 90.  to check if a triple is present.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int arr_size;
+    int array1[] = {1, 1, 2, 2, 1};
+    arr_size = sizeof(array1)/sizeof(array1[0]);
+    printf("%d",test(array1, arr_size));
+    int array2[] = {1, 1, 2, 1, 2, 3};
+    arr_size = sizeof(array2)/sizeof(array2[0]);
+    printf("\n%d",test(array2, arr_size));
+    int array3[] = {1, 1, 1, 2, 2, 2, 1 };
+    arr_size = sizeof(array3)/sizeof(array3[0]);
+    printf("\n%d",test(array3, arr_size));
+    }
+    int test(int nums[], int arr_size)
+         {
+             for (int i = 0; i < arr_size - 1; i++)
+            {
+                 if(nums[i] == nums[i + 1] && nums[i + 2] == nums[i])
+                     return 1;
+            }
+           return 0;
+
+        }
