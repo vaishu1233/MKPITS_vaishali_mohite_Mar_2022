6db098923d69e9e0495227be5c11eae9f8f8e290
vaishali mohite <101799368+vaishu1233@users.noreply.github.com>
2022-05-10 11:33:09 +0530

    Add files via upload

---
+++ 97. y is greater than x and z.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){
+    printf("%d",test(1, 2, 3));
+    printf("\n%d",test(4, 5, 6));
+    printf("\n%d",test(-1, 1, 0));
+    }
+    int test(int x, int y, int z)
+         {
+           return x < y && y < z;
+         }
