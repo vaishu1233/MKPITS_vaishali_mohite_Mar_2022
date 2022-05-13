71020b883485b12711033043e853b2dd3cc628b9
vaishali mohite <101799368+vaishu1233@users.noreply.github.com>
2022-05-13 11:49:34 +0530

    Add files via upload

---
+++ 123. to check if the sum of all 5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int arr_size;
+    int array1[] = {1, 5, 6, 9, 10, 17};
+    arr_size = sizeof(array1)/sizeof(array1[0]);
+    printf("%d",test(array1, arr_size));
+    int array2[] = {1, 5, 5, 5, 10, 17 };
+    arr_size = sizeof(array2)/sizeof(array2[0]);
+    printf("\n%d",test(array2, arr_size));
+    int array3[] = {1, 1, 5, 5, 5, 5};
+    arr_size = sizeof(array3)/sizeof(array3[0]);
+    printf("\n%d",test(array3, arr_size));
+  }
+  int test(int nums[], int arr_size)
+         {
+            int sum = 0;
+
+            for (int i = 0; i < arr_size; i++)
+            {
+                if (nums[i] == 5)
+                    sum += 5;
+            }
+            return sum == 15;
+         }
