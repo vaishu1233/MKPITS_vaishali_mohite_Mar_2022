332a12ecd28b33a33098befba07be956156b4311
vaishali mohite <101799368+vaishu1233@users.noreply.github.com>
2022-05-13 11:49:58 +0530

    Add files via upload

---
+++ 124. check if the number of three is greater.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int arr_size;
+    int array1[] = {1, 5, 6, 9, 3, 3};
+    arr_size = sizeof(array1)/sizeof(array1[0]);
+    printf("%d",test(array1, arr_size));
+    int array2[] = {1, 5, 5, 5, 10, 17};
+    arr_size = sizeof(array2)/sizeof(array2[0]);
+    printf("\n%d",test(array2, arr_size));
+    int array3[] = {1, 3, 3, 5, 5, 5};
+    arr_size = sizeof(array3)/sizeof(array3[0]);
+    printf("\n%d",test(array3, arr_size));
+  }
+
+  int test(int nums[], int arr_size)
+         {
+            int no_3 = 0, no_5 = 0;
+
+            for (int i = 0; i < arr_size; i++)
+            {
+                if (nums[i] == 3) no_3++;
+                if (nums[i] == 5) no_5++;
+            }
+
+            return no_3 > no_5;
+         }
