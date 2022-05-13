2190ae7ef097cc0de59f2280da1f8ffa81768b5d
vaishali mohite <101799368+vaishu1233@users.noreply.github.com>
2022-05-13 13:19:16 +0530

    Add files via upload

---
+++ WAP to Enter 5 array Element .c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+while(beg<=end);
+mid=big+end/2;
+if(ser==a[mid])
+{
+    present
+    break;
+}else
+if(ser<=[mid])
+    end=mid-1;
+{
+    end=mid-1;
+}
+else
+{
+    beg=mid=1;
+}
