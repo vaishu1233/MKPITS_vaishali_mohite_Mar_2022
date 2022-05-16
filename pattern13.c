a2474b8393bd2c4156636fbf19edc47df8ba7c5a
vaishali mohite <101799368+vaishu1233@users.noreply.github.com>
2022-05-16 11:29:01 +0530

    Add files via upload

---
+++ pattern13.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i, j, n;
+printf("Enter number of rows: ");
+scanf("%d",&n);
+for(i = 1; i <= n; i++)
+{
+for(j = 1; j <= i; j++)
+{
+printf("* ");
+}
+//Ending line after each row
+printf("\n");
+}
+return 0;
+}
